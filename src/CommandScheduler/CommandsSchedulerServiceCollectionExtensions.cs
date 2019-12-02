58e68d90c494e68ad08277e2fef9c66951b2f94f
AliBayat <ali_programmer88@hotmail.com>
2019-12-02 15:41:59 +0330

    Add CommandsSchedulerServiceCollectionExtensions

---
+++ src/CommandScheduler/CommandsSchedulerServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CommandSchedular;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandScheduler
+{
+    public static class CommandsSchedulerServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCommandsScheduler(this IServiceCollection services)
+        {
+            services.AddScoped<CommandsExecutor>();
+            services.AddScoped<AsyncCommand>();
+
+            return services;
+        }
+    }
+}
